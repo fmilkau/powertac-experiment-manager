d9e9612a43da3154771a18d8066e7c31c66c564e
Frederik Milkau <fmilkau@uni-koeln.de>
2023-04-28 18:52:25 +0200

    updated config; added control scripts; updated installation routine

---
+++ bin/remove.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+path_bin=$(dirname "$(realpath "$0")")
+path_em=$(dirname "$path_bin")
+path_config="${path_em}/config"
+
+source "${path_bin}/util.sh"
+
+compose_command="$(em_compose_command)"
+
+echo "Removing containers ..."
+$compose_command \
+  --project-directory "${path_config}" \
+  --file "${path_config}/experiment-manager.compose.yml" \
+  --env-file "${path_config}/experiment-manager.env" \
+  --project-name "pem" \
+  down
+
+if [ "$1" == "--remove-files" ] || [ "$1" == "-r" ]; then
+  echo "Removing files ..."
+  rm -r "$path_em"
+fi
---
+++ bin/stop.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+path_bin=$(dirname "$(realpath "$0")")
+path_em=$(dirname "$path_bin")
+path_config="${path_em}/config"
+
+source "${path_bin}/util.sh"
+
+compose_command="$(em_compose_command)"
+
+echo "Stopping containers ..."
+$compose_command \
+  --project-directory "${path_config}" \
+  --file "${path_config}/experiment-manager.compose.yml" \
+  --env-file "${path_config}/experiment-manager.env" \
+  --project-name "pem" \
+  stop
